d1f3f07bbd72b4c49be195e18a6eb2dc97a20f11
unknown <jose.oreo99@gmail.com>
2020-10-15 16:51:08 -0600

    Llenado de tabla de análisis

--- ProyectCompis2/ProyectCompis2/Models/AnálisisSintactico.cs
+++ ProyectCompis2/ProyectCompis2/Models/AnálisisSintactico.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using ProyectCompis2.Models;
 using System.Threading.Tasks;
 
 namespace ProyectCompis2
@@ -29,27 +31,52 @@ namespace ProyectCompis2
 
         public void LlenarTabla(List<string[]> tokenList)
         {
-            Dictionary<string, string[]> simbolAction = new Dictionary<string, string[]>();
-            //List<string> simbolos = new List<string> { ";","Identificador","const", "int", "double", "bool", "string",
-            //"[]", "(", ")", "void", ",", "class", "{", "}", ":", "interface", "if", "else", "while", "for", "return", "break",
-            //"Console", ".","WriteLine", "=", "this", "+", "*", "%", "-","<","<=" , "==", "&&" , "!", "New" , "intCon"
-            //,"doubleCon", "boolCon", "stringCon", "null" , "$", "Program", "Decl" , "VariableDecl", "Variable", "ConstDecl"
-            //, "ConstType", "Type" , "Type_P", "Type_R", "FunctionsDecl" , "Formals", "Formals_P", "ClassDecl", "ClassDecl_P" , "ClassDecl_R" ,
-            //  "ClassDecl_O", "ClassDecl_Q", "Field" , "InterfaceDecl", "InterfaceDecl_P" ,"Prototype" , "StmtBlock", "StmtBlock_P" , "StmtBlock_R"
-            //, "StmtBlock_O" , "Stmt", "Stmt_P" , "IfStmt" , "IfStmt_P", "WhileStmt" , "ForStmt" , "ReturnStmt", "BreakStmt", "PrintStmt", "PrintStmt_P", "Expr" , "LValue", "Constant"};
-            Simbolos simbol = new Simbolos("Identificador", "s19", "", 0);
-            simbol = new Simbolos("const", "s11", "", 0);
-            simbol = new Simbolos("int", "s15", "", 0);
-            simbol = new Simbolos("double", "s16", "", 0);
-            simbol = new Simbolos("bool", "s17", "", 0);
-            simbol = new Simbolos("string", "s18", "", 0);
-            simbol = new Simbolos("void", "s10", "", 0);
-            simbol = new Simbolos("class", "s12", "", 0);
-            simbol = new Simbolos("interface", "s13", "", 0);
-            simbol = new Simbolos("Program", "1", "", 0);
-            simbol = new Simbolos("Decl", "2", "", 0);
+            List<string> simbolos = new List<string> { ";","Identificador","const", "int", "double", "bool", "string",
+            "[]", "(", ")", "void", ",", "class", "{", "}", ":", "interface", "if", "else", "while", "for", "return", "break",
+            "Console", ".","WriteLine", "=", "this", "+", "*", "%", "-","<","<=" , "==", "&&" , "!", "New" , "intCon"
+            ,"doubleCon", "boolCon", "stringCon", "null" , "$", "Program", "Decl" , "VariableDecl", "Variable", "ConstDecl"
+            , "ConstType", "Type" , "Type_P", "Type_R", "FunctionsDecl" , "Formals", "Formals_P", "ClassDecl", "ClassDecl_P" , "ClassDecl_R" ,
+              "ClassDecl_O", "ClassDecl_Q", "Field" , "InterfaceDecl", "InterfaceDecl_P" ,"Prototype" , "StmtBlock", "StmtBlock_P" , "StmtBlock_R"
+            , "StmtBlock_O" , "Stmt", "Stmt_P" , "IfStmt" , "IfStmt_P", "WhileStmt" , "ForStmt" , "ReturnStmt", "BreakStmt", "PrintStmt", "PrintStmt_P", "Expr" , "LValue", "Constant"};
 
-            dicGeneral.Add(0, simbolAction);
+            string path = System.IO.Directory.GetCurrentDirectory() + "\\Tabla.txt";
+            var numEstado = 0;
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        Dictionary<string, string[]> simbolAction = new Dictionary<string, string[]>();
+                        var estado = reader.ReadLine();
+                        var separar = estado.Split(';');
+                        Simbolos simbol = new Simbolos();
+                        var contSimbol = 0;
+                        foreach (var item in simbolos)
+                        {
+                            if(separar[contSimbol] != "n")
+                            {
+                                string[] valores = new string[2];
+                                if (separar[contSimbol].Contains("/"))
+                                {
+                                    var dosCaminos = separar[contSimbol].Split('/');
+                                    valores[0] = dosCaminos[0];
+                                    valores[1] = dosCaminos[1];
+                                }
+                                else
+                                {
+                                    valores[0] = separar[contSimbol];
+                                    valores[1] = null;
+                                }
+                                simbolAction.Add(item, valores);
+                            }
+                            contSimbol++;
+                        }
+                        dicGeneral.Add(numEstado, simbolAction);
+                        numEstado++;
+                    }
+                }
+            }
             
         }
         public void Parse_Program()
--- ProyectCompis2/ProyectCompis2/Models/Simbolos.cs
+++ ProyectCompis2/ProyectCompis2/Models/Simbolos.cs
@@ -8,21 +8,18 @@ namespace ProyectCompis2
 {
     class Simbolos
     {
-
         public Dictionary<string, string[]> simbolAction = new Dictionary<string, string[]>();
-        Dictionary<int, Dictionary<string, string[]>> dicGeneral = new Dictionary<int, Dictionary<string, string[]>>();
         public  string[] vector = new string[2];
 
-        public  Simbolos(string NombreSimbolo, string ValorV1,string ValorV2, int Estado)
+        public void Simbol(string NombreSimbolo, string ValorV1,string ValorV2, int Estado)
         {
             vector[0] = ValorV1;
             vector[1] = ValorV2;
             simbolAction.Add(NombreSimbolo, vector);
-            dicGeneral.Add(Estado, simbolAction);
         }
-        public Dictionary<int, Dictionary<string, string[]>> ObtenerDicGeneral()
+        public Dictionary<string, string[]> ObtenerDic()
         {
-            return dicGeneral;
+            return simbolAction;
         }
 
     }
